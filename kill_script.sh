d634920d44090b8c597582073c46f637f4061cf8
Husnul <72526264+hackeratjeh@users.noreply.github.com>
2020-11-09 16:55:54 +0700

    Add files via upload

---
+++ kill_script.sh
@@ -0,0 +1,63 @@
+#/bin/bash kill_script.sh
+#kill script
+
+figlet "HACKER-ACEH" | lolcat
+echo "=============================================="
+echo "AUTHER  = HACKER ACEH"  | lolcat
+echo "IS TEAM = TERMUX-CYBER-TEAM" |  lolcat
+echo "MY-WA   = +62 877-1454-5505"  | lolcat 
+echo "=============================================="
+
+#MENAMPILKAN MENU PERTAMA
+
+echo  "SELAMAT DATANG" | lolcat
+echo "< 1 > script 6VIRUS" | lolcat
+echo "< 2 > script MENGUBAH TAMPILAN AWAL" | lolcat
+echo "< 3 > script SETAN" |lolcat
+echo "< 4 > script NIK-KK" | lolcat
+echo "< 5 > script SPAM_WA" | lolcat
+echo "< 6 > script FRAMEWORK" | lolcat
+echo 
+read -p "SILAHKAN PILIH MENU= " pilih | lolcat
+#batas
+if [[ $pilih > 1 ]]  ||  [[ $pilih > 1 ]]
+git clone https://github.com/novalattasya/6VIRUS
+cd 6VIRUS
+bash 6VIRUS.sh
+fi
+#batas
+if [[ $pilih > 2 ]]  ||  [[ $pilih > 2 ]]
+git clone https://github.com/Din-zUgex95/style
+cd style
+python2 termux.py
+fi
+#batas
+if [[ $pilih > 3 ]]  ||  [[ $pilih > 3 ]]
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2  SETAN.py
+fi
+#batas
+if [[ $pilih > 4 ]]  ||  [[ $pilih > 4 ]]
+git clone https://github.com/afokay/nik-kk
+cd nik-kk
+
+fi
+#batas
+if [[ $pilih > 5 ]]  ||  [[ $pilih > 5 ]]
+git clone https://github.com/RuangCoder/spam_wa
+cd spam_wa
+pyrhon2 wa.py
+fi
+#batas
+if [[ $pilih > 6 ]]  ||  [[ $pilih > 6 ]]
+git clone https://github.com/LOoLzeC/Evil-create-framework.git
+cd Evil-create-framework
+bash install.sh
+vcrt = help
+vcrt = create virus /
+vcrt = SET OUTPUT
+vcrt = SETVIRUS NAME /
+vcrt = run
+fi
+
